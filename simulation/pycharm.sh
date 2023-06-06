26a5fb5533f3b6da972b407bcacf75086ce2e977
github <novakfi8@fel.cvut.cz>
2023-06-06 16:19:48 +0200

    update pycharm script for year 2023

--- simulation/pycharm.sh
+++ simulation/pycharm.sh
@@ -5,4 +5,4 @@ MY_PATH=`dirname "$0"`
 MY_PATH=`( cd "$MY_PATH" && pwd )`
 cd $MY_PATH
 
-./singularity.sh exec "source ~/.bashrc && /opt/pycharm-community-2022.1.3/bin/pycharm.sh ~/summer-school-2022/mrim_task/mrim_planner/ > /dev/null 2>&1 &"
+./singularity.sh exec "source ~/.bashrc && /opt/pycharm-community-2022.1.3/bin/pycharm.sh ~/summer-school-2023/mrim_task/mrim_planner/ > /dev/null 2>&1 &"
